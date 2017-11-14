9bdbe301c7a85fe0afc2076854f5adf5469ffdb0
Celia FUSARO <cfusaro@e2r13p13.42.fr>
2017-11-14 06:51:36 +0100

    j06

---
+++ ex02/ft_print_params.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfusaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/13 17:45:06 by cfusaro           #+#    #+#             */
+/*   Updated: 2017/09/17 18:49:55 by cfusaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+int		main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
---
+++ ex04/ft_sort_params.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfusaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/13 19:30:52 by cfusaro           #+#    #+#             */
+/*   Updated: 2017/09/16 18:23:19 by cfusaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	ft_putchar(char c);
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] < s2[i])
+			return (-1);
+		if (s1[i] > s2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char *c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+void	ft_putstr(char *str)
+{
+	int c;
+
+	c = 0;
+	while (str[c])
+	{
+		ft_putchar(str[c]);
+		c++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			ft_swap(&argv[i], &argv[i + 1]);
+			i = 1;
+		}
+		else
+			i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
